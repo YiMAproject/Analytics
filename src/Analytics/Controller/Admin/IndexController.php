4dae993181eddbb97598614bd3138d1e9d0520b2
Payam Naderi <naderi.payam@gmail.com>
2014-09-13 16:50:53 +0430

    init test

--- src/Analytics/Controller/Admin/IndexController.php
+++ src/Analytics/Controller/Admin/IndexController.php
@@ -12,6 +12,20 @@ class IndexController extends AbstractActionController
 {
     public function dashboardAction()
     {
+        /** @var $client \Analytics\Service\Client\Google */
+        $client = $this->serviceLocator->get('Analytics.Client');
+
+        $code = '4/T-cRDBtr9DbwrzXgwCRShd2a19tq.QkGENyBta8kWyjz_MlCJoi0sWOXdkAI';
+        if ($code) {
+            $client->authorize($code);
+
+            d_e('Authorized');
+        }
+
+        if (!$client->isAuthorized())
+            $this->redirect()->toUrl($client->getAuthUrl());
+
+
 
     }
 }
